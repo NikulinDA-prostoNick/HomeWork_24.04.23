f851f4811f2d8b7b4a5c392e28cee7bf465b6452
Nikulin Denis <nikulin_denis_a@mail.ru>
2023-04-26 15:12:20 +0300

    Done

--- HomeWork_24.04.23/Program.cs
+++ HomeWork_24.04.23/Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// int Prompt(string msg)
+// {
+// System.Console.WriteLine(msg);
+// return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int number = Prompt("Введите трехзначное число");
+// if (number >=100 && number <1000)
+
+// {
+// int mid = (number % 100 - number % 10) / 10;
+// System.Console.WriteLine($"Вторая цифра числа {number} является {mid}");
+// }
+
+// else
+// System.Console.WriteLine("Это не трехзначное число");
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// int Prompt(string msg)
+// {
+// System.Console.WriteLine(msg);
+// return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int number = Prompt("Введите число");
+// int fnumber(int number)
+// {
+//     while (number > 999)
+//     {
+//         number /= 10;
+//     }
+// return number % 10;
+// }
+
+// bool check(int number)
+// {
+//     if (number < 100)
+//     return false;
+//     else return true;
+// }
+
+// if (check(number) != true)
+// System.Console.WriteLine("Третьей цифры нет");
+// else
+// System.Console.WriteLine($"Третьей цифрой числа {number} является {fnumber(number)}");
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+int Prompt(string msg)
+{
+
+System.Console.WriteLine(msg);
+return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите число");
+string x = "Это не день недели";
+string check(int number)
+{
+if (number >= 6 && number < 8) x = "Это выходной";
+if (number >= 1 && number < 7) x = "Это будний день";
+return x;
+}
+System.Console.WriteLine(check(number));
